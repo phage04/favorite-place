06012d140b11fbed362f8f33ff552c2bb1327889
lylejover <lylejover2yahoo.com>
2016-02-16 00:50:29 +0800

    v1 Complete

---
+++ Favorite Place/DetailVC.swift
@@ -0,0 +1,27 @@
+//
+//  DetailVC.swift
+//  Favorite Place
+//
+//  Created by Lyle Christianne Jover on 16/02/2016.
+//  Copyright © 2016 Lyle Christianne Jover. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+   
+    }
+    
+    
+    @IBAction func backPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+
+}
--- Favorite Place/ViewController.swift
+++ Favorite Place/ViewController.swift
@@ -9,16 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var moreBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        moreBtn.layer.cornerRadius = 2.0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
